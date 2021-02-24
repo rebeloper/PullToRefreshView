7fddf55d67c16077e25bc9724a8e4571bab72254
Alex Nagy <rebeloper@users.noreply.github.com>
2021-02-24 17:53:41 +0200

    pull to refresh view

--- Sources/PullToRefreshView/PullToRefreshView.swift
+++ Sources/PullToRefreshView/PullToRefreshView.swift
@@ -1,3 +1,59 @@
-struct PullToRefreshView {
-    var text = "Hello, World!"
+//
+//  PullToRefreshView.swift
+//  
+//
+//  Created by Alex Nagy on 24.02.2021.
+//
+
+import SwiftUI
+
+@available(iOS 14.0, *)
+public struct PullToRefreshView<RefreshView: View>: View {
+    
+    public var coordinateSpaceName: String
+    public var refreshView: () -> RefreshView
+    public var onRefresh: () -> Void
+    
+    @State public var needRefresh: Bool = false
+    
+    public init(coordinateSpaceName: String, refreshView: @escaping () -> RefreshView, onRefresh: @escaping () -> Void) {
+        self.coordinateSpaceName = coordinateSpaceName
+        self.refreshView = refreshView
+        self.onRefresh = onRefresh
+    }
+    
+    public init(coordinateSpaceName: String, onRefresh: @escaping () -> Void) {
+        self.coordinateSpaceName = coordinateSpaceName
+        self.refreshView = { Image(systemName: "arrow.down") as! RefreshView }
+        self.onRefresh = onRefresh
+    }
+    
+    public var body: some View {
+        GeometryReader { geo in
+            if (geo.frame(in: .named(coordinateSpaceName)).midY > 50) {
+                Spacer()
+                    .onAppear {
+                        needRefresh = true
+                    }
+            } else if (geo.frame(in: .named(coordinateSpaceName)).maxY < 10) {
+                Spacer()
+                    .onAppear {
+                        if needRefresh {
+                            needRefresh = false
+                            onRefresh()
+                        }
+                    }
+            }
+            HStack {
+                Spacer()
+                if needRefresh {
+                    ProgressView()
+                } else {
+                    refreshView()
+                }
+                Spacer()
+            }
+        }.padding(.top, -50)
+    }
 }
+
--- Tests/PullToRefreshViewTests/PullToRefreshViewTests.swift
+++ Tests/PullToRefreshViewTests/PullToRefreshViewTests.swift
@@ -6,7 +6,6 @@ final class PullToRefreshViewTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(PullToRefreshView().text, "Hello, World!")
     }
 
     static var allTests = [
